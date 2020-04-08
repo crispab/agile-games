43ae9bde023bc987fc4eae0c60ba60d8fe03810a
Per Lundholm <per@artcomputer.se>
2020-04-08 08:36:30 +0200

    Game session id is readable number between 0 and 999999 expressed as two groups of three digits.

--- src/main/elm/src/Page/FacilitatorPage.elm
+++ src/main/elm/src/Page/FacilitatorPage.elm
@@ -2,7 +2,7 @@ module Page.FacilitatorPage exposing (viewFacilitatorPage)
 
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
-import Html exposing (Html, div, h1, text)
+import Html exposing (Html, div, h1, h2, text)
 import Html.Attributes exposing (class, style)
 import Model exposing (Model)
 import Msg exposing (Msg(..))
@@ -13,14 +13,15 @@ viewFacilitatorPage model =
     Grid.container []
         [ Grid.row []
             [ Grid.col [ Col.sm ]
-                [ viewHeadLine
+                [ viewHeadLine model.gameSessionId
                 ]
             ]
         ]
 
 
-viewHeadLine : Html Msg
-viewHeadLine =
+viewHeadLine : String -> Html Msg
+viewHeadLine gameSessionID =
     div [ class "jumbotron", style "text-align" "center" ]
         [ h1 [] [ text "Facilitator" ]
+        , h2 [] [ text <| "Game id: " ++ gameSessionID ]
         ]
--- src/main/java/agile/games/tts/GameSessionId.java
+++ src/main/java/agile/games/tts/GameSessionId.java
@@ -1,4 +1,19 @@
 package agile.games.tts;
 
-public class GameSessionId extends AbstractId {
+import java.util.Random;
+
+public class GameSessionId  {
+    private static Random random = new Random();
+    private final String id;
+
+    public GameSessionId() {
+        int nextInt = random.nextInt(999999);
+        String tmpStr = String.format("%06d", nextInt);
+        id = tmpStr.substring(0,3) + " " + tmpStr.substring(3);
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
 }
