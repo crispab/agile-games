227988e431112277522b240fc43f64ff070f7aa2
Per Lundholm <per@artcomputer.se>
2020-04-21 16:28:24 +0200

    Changed the avatar set by replacing it with 30 animals.

--- src/main/java/agile/games/tts/AvatarAssigner.java
+++ src/main/java/agile/games/tts/AvatarAssigner.java
@@ -7,12 +7,43 @@ public class AvatarAssigner {
     private final List<String> avatars;
     private int avatarIndex = 0;
 
-    public AvatarAssigner() {
-        avatars = Arrays.asList("circle.png", "heart.png", "sun.png");
-    }
-
     public String nextAvatar() {
         avatarIndex = (avatarIndex + 1) % avatars.size();
         return avatars.get(avatarIndex);
+    }
+
+    public AvatarAssigner() {
+        avatars = Arrays.asList(
+                "bear.png",
+                "buffalo.png",
+                "chick.png",
+                "chicken.png",
+                "cow.png",
+                "crocodile.png",
+                "dog.png",
+                "duck.png",
+                "elephant.png",
+                "frog.png",
+                "giraffe.png",
+                "goat.png",
+                "gorilla.png",
+                "hippo.png",
+                "horse.png",
+                "monkey.png",
+                "moose.png",
+                "narwhal.png",
+                "owl.png",
+                "panda.png",
+                "parrot.png",
+                "penguin.png",
+                "pig.png",
+                "rabbit.png",
+                "rhino.png",
+                "sloth.png",
+                "snake.png",
+                "walrus.png",
+                "whale.png",
+                "zebra.png"
+        );
     }
 }
